75a6f5cebf1df548b0261c4aae1f7caa347776cb
Precious <preciousonome002@gmail.com>
2024-06-27 17:02:54 +0100

    djangoProject

--- account/models.py
+++ account/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.conf import settings
 from .utils import generate_account_number
 from .validators import validate_pin
 
@@ -7,6 +9,7 @@ from .validators import validate_pin
 
 
 class Account(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     account_number = models.CharField(max_length=10,
                                       default=generate_account_number, unique=True,
                                       primary_key=True)
