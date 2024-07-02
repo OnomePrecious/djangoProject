80a33e1241cd06570aa8d6c32d1ef19e16cf25a8
Precious <preciousonome002@gmail.com>
2024-07-02 15:25:46 +0100

    djangoProject

--- account/signals.py
+++ account/signals.py
@@ -4,5 +4,8 @@ from user.models import User
 from .models import Account
 
 
-def create_account(self, instance, sender):
-    pass
+@receiver(signal=post_save)
+def create_account(self, created, instance, sender=User):
+    if created:
+        Account.objects.create(user=instance, account_number=instance.phone[1:])
+
