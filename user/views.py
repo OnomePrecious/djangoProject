04b77e9d50b1f7398bcc71fd1828aea7d2f2871c
Precious <preciousonome002@gmail.com>
2024-07-02 11:43:22 +0100

    djangoProject

--- user/views.py
+++ user/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+
+from .models import User
+from .serializers import UserCreateSerializer
+
 
 # Create your views here.
+
+class RegisterUser(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserCreateSerializer
+
