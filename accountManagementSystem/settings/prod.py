f3f3a3d6b1bb02f41d9723b7a356c0bc6f7852c0
Precious <preciousonome002@gmail.com>
2024-07-05 11:50:27 +0100

    djangoProject

--- accountManagementSystem/settings/prod.py
+++ accountManagementSystem/settings/prod.py
@@ -15,3 +15,12 @@ DATABASES = {
 
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL = 'info@jagudabank.com'
+
+
